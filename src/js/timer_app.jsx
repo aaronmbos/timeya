697c81a2d4ed6c09ca4b939d562b412409921ecd
aaronmbos <aaronmbos@gmail.com>
2020-07-22 22:53:01 -0500

    Begin implementing add new timer button by implementing click handler and setting timer state

--- src/js/timer_app.jsx
+++ src/js/timer_app.jsx
@@ -1,59 +1,51 @@
 const App = () => {
-    const [showModal, setShowModal] = React.useState(false);
+  [timers, setTimers] = React.useState([]);
 
-    const handleShowModal = () => {
-        setShowModal(true);
-    }  
+  const handleAddTimer = () => {
+    const id = timers.length + 1;
+    setTimers(timers.concat([{
+      id: id,
+      name: "",
+      status: ""
+    }]));
+  };
 
-    return (
-        <div>
-            <AddModal showModal={showModal} />
-            <AddTimer handleShowModal={handleShowModal} />
-            <TimerContainer timers={timers} />
-        </div>
-    );
-}
+  return (
+    <div>
+      <ActionRow handleAddTimer={handleAddTimer} />
+      <TimerContainer timers={timers} /> 
+    </div>
+  );
+};
 
-const AddTimer = (props) => {
-    return <button onClick={() => props.handleShowModal()}>Add Timer</button>
-}
-
-const AddModal = (props) => {
-    const isModalActive = props.showModal ? ' is-active' : '';
-    return (
-        <div className={`modal${isModalActive}`}>
-            <div className="modal-background"></div>
-            <div className="modal-card">
-                <header className="modal-card-head">
-                    <p className="modal-card-title">Modal title</p>
-                    <button className="delete" aria-label="close"></button>
-                </header>
-                <section className="modal-card-body">
-                    {/*content*/}
-                </section>
-                <footer className="modal-card-foot">
-                    <button className="button is-success">Save changes</button>
-                    <button className="button">Cancel</button>
-                </footer>
-            </div>
-        </div>
-    )
-}
+const ActionRow = (props) => {
+  return (
+    <div className="action-row buttons are-small">
+      <button
+        className="button is-success"
+        onClick={() => props.handleAddTimer()}
+      >
+        <span className="icon is-small">
+          <i className="fas fa-plus"></i>
+        </span>
+        <span>New Timer</span>
+      </button>
+    </div>
+  );
+};
 
 const TimerContainer = (props) => {
-    return (
-        <div>
-            {props.timers.map((prop) => <TimerCard key={prop.id} name={prop.val} />)}
-        </div>
-    );
-}
+  return (
+    <div>
+      {props.timers.map((prop) => <TimerCard key={prop.id} name={prop.name} />)}
+    </div>
+  );
+};
 
 const TimerCard = (props) => {
-    return (
-        <div className="timer-card">
-            This is a card from {props.name}.
-        </div>
-    );
-}
+  return (
+    <div className="timer-card">This is a card from {props.name}.</div>
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
