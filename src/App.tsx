591a1f5fc7e8366df0e9f65393f3a72e0194513c
Aaron Bos <aaronmbos@gmail.com>
2022-02-27 23:02:21 -0600

    Add new component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppHeader from './components/AppHeader'
 
 function App() {
   return (
     <div className="App">
+      <AppHeader />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
